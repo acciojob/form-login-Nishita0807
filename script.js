e3e2f53e53e116d2c612fef4e9c090903b84e197
Nishita Rani <92663360+Nishita0807@users.noreply.github.com>
2023-09-18 15:24:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 function getFormvalue() {
     //Write your code here
+  var form = document.getElementById("form1");
 
+    // Get the values of the first name and last name input fields
+    var firstName = form.elements.fname.value;
+    var lastName = form.elements.lname.value;
+
+    // Check if both fields are filled
+    if (firstName && lastName) {
+        // Concatenate the first and last name
+        var fullName = firstName + " " + lastName;
+
+        // Display the full name using an alert
+        alert(fullName);
+    } else {
+        // If one or both fields are empty, show an error message
+        alert("Please fill in both the first name and last name fields.");
+    }
+
+    // Prevent the form from submitting and refreshing the page
+    return false;
 }
